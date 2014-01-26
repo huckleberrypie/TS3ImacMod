de512de756597d54c07b759fc4dd03b5d40ad559
huckleberrypie <peanuthead_069@yahoo.com>
2014-01-26 22:01:26 +0800

    Changed namespace and updated .package
    Ditto.

--- Huck.Sims3.ComputerImac/Huck.Sims3.ComputerImac/ComputerImac.cs
+++ Huck.Sims3.ComputerImac/Huck.Sims3.ComputerImac/ComputerImac.cs
@@ -1,4 +1,4 @@
-﻿/*   This program is free software: you can redistribute it and/or modify
+﻿/*  This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
@@ -15,7 +15,7 @@ using Sims3.Gameplay.Abstracts;
 using Sims3.Gameplay.ObjectComponents;
 using Sims3.SimIFace;
 using System;
-namespace Sims3.Gameplay.Objects.Electronics
+namespace Sims3.Gameplay.Objects.Electronics.Blakemod
 {
     public class ComputerImac : ComputerExpensive
     {
