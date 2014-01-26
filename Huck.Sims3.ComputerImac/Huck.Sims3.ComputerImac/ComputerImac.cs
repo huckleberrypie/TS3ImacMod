14d7cafadc67861fe809a94ffcbad839969c672a
huckleberrypie <peanuthead_069@yahoo.com>
2014-01-26 10:46:59 +0800

    Initial release
    Hmm, nothing much really, although I might as well tweak the object a
    bit and/or add some stuff to the code.

---
+++ Huck.Sims3.ComputerImac/Huck.Sims3.ComputerImac/ComputerImac.cs
@@ -0,0 +1,16 @@
+﻿using Sims3.Gameplay.Abstracts;
+using Sims3.Gameplay.ObjectComponents;
+using Sims3.SimIFace;
+using System;
+namespace Sims3.Gameplay.Objects.Electronics
+{
+    public class ComputerImac : ComputerExpensive
+    {
+        public override void TurnOnLightEffect()
+        {
+            {
+                return;
+            }
+        }
+    }
+}
